a67ab3d565b22bdc40dbac2bdc8922480c820b96
Omid Toloei <omidtoloei.dev@gmail.com>
2026-07-14 21:36:45 +0330

    Add files via upload

---
+++ javascript/app.js
@@ -0,0 +1,209 @@
+/* =========================
+Global Variables
+========================= */
+
+// api response array
+let users = [];
+let posts = [];
+let comments = [];
+
+let isLoading = false;
+
+/* =========================
+DOM Elements
+========================= */
+
+const userPostsModalParent = document.querySelector(".user-posts-modal-overlay");
+const userPostCommentParent = document.querySelector(".user-post-comment-modal-overlay");
+const showLoading = document.querySelector("#showLoading");
+const tableBody = document.querySelector("#tableBody");
+const userPostsModalOverlay = document.querySelector(".user-posts-modal-overlay");
+const userProfileName = document.querySelector("#userProfileName");
+const userProfileUsername = document.querySelector("#userProfileUsername");
+const userProfileEmail = document.querySelector("#userProfileEmail");
+const userProfilePhone = document.querySelector("#userProfilePhone");
+const userProfilePosts = document.querySelector("#userProfilePosts");
+const postTitle = document.querySelector(".post-title");
+const postBody = document.querySelector(".post-body");
+
+const postCommentsUl = document.querySelector("#postCommentsUl");
+const userCommentName = document.querySelector(".user-comment-name");
+const userCommentEmail = document.querySelector(".user-comment-email");
+const userComment = document.querySelector(".user-comment");
+
+/* =========================
+Event Listeners
+========================= */
+
+userPostsModalParent.addEventListener("click", (event) => {
+  if (event.target === userPostsModalParent) {
+    userPostsModalParent.style.display = "none";
+  }
+});
+
+userPostCommentParent.addEventListener("click", (event) => {
+  if (event.target === userPostCommentParent) {
+    userPostCommentParent.style.display = "none";
+  }
+});
+
+/* =========================
+API
+========================= */
+
+const getFetchData = async (urlAddress) => {
+  if (isLoading) return;
+
+  try {
+    isLoading = true;
+
+    showLoading.style.display = "flex";
+
+    const response = await fetch(urlAddress);
+
+    if (!response.ok) {
+      switch (response.status) {
+        case 404:
+          throw new Error(`HTTP ${response.status}: data not found!`);
+        case 500:
+          throw new Error(`HTTP ${response.status}: internal server error!`);
+        case 503:
+          throw new Error(
+            `HTTP ${response.status}: service unavailable. try again later!`,
+          );
+        default:
+          throw new Error(`HTTP ${response.status}: Something went wrong!`);
+      }
+    }
+
+    const dataJson = await response.json();
+
+    return dataJson;
+
+  } catch (error) {
+    if (!navigator.onLine) {
+      alert("Please check your internet connection!");
+      return;
+    }
+    if (error instanceof TypeError) {
+      alert("A network error occurred!");
+      return;
+    }
+    alert(error.message);
+  } finally {
+    isLoading = false;
+    showLoading.style.display = "none";
+  }
+}
+
+const getUsers = async () => {
+  let apiUsersResponse = await getFetchData("https://jsonplaceholder.typicode.com/users");
+  users.push(...apiUsersResponse);
+}
+const getPosts = async () => {
+  let apiPostsResponse = await getFetchData("https://jsonplaceholder.typicode.com/posts");
+  posts = apiPostsResponse;
+}
+const getComments = async () => {
+  let apiCommentsResponse = await getFetchData("https://jsonplaceholder.typicode.com/comments");
+  comments = apiCommentsResponse;
+}
+
+/* =========================
+Functions
+========================= */
+
+async function run() {
+  await getUsers();
+  showUsers();
+}
+
+run();
+
+/* =========================
+Render Functions
+========================= */
+
+function showUsers() {
+  tableBody.innerHTML = "";
+
+  let userTableStructure = "";
+  users.forEach((element) => {
+    userTableStructure += `<tr title="click to see user profile" data-user-id="${element.id}" class="user-table-row">
+    <td>${element.name}</td>
+    <td>${element.address.city}</td>
+    </tr>`;
+  });
+
+  tableBody.innerHTML = userTableStructure;
+
+  tableBody.addEventListener("click", (event) => {
+    const row = event.target.closest(".user-table-row");
+
+    if (!row) return;
+
+    showPosts(row.dataset.userId);
+  });
+}
+
+async function showPosts(userId) {
+  await getPosts();
+  userPostsModalOverlay.style.display = "flex";
+
+  const user = users.find(user => user.id === Number(userId));
+
+  userProfileName.textContent = user.name;
+  userProfileUsername.innerHTML = `<img src="icons/at-icon.png" /> ${user.username}`;
+  userProfileEmail.innerHTML = `<img src="icons/email-icon.png" /> ${user.email}`;
+  userProfilePhone.innerHTML = `<img src="icons/phone-icon.png" /> ${user.phone}`;
+
+
+  userProfilePosts.innerHTML = "";
+  let newPostTitleStructure = "";
+
+  let userPosts = posts.filter((object) => {
+    return object.userId == userId;
+  });
+
+  userPosts.forEach((element) => {
+    newPostTitleStructure += `<li class="user-profile-post-title" title="click to read post" data-post-id="${element.id}">
+    <p href="#" class="post-title-profile-paragraph">${element.title}</p>
+    </li>`;
+  });
+
+  userProfilePosts.innerHTML = newPostTitleStructure;
+
+  userProfilePosts.addEventListener("click", (event) => {
+    const row = event.target.closest(".user-profile-post-title");
+    if (!row) return;
+    showComments(row.dataset.postId);
+  });
+}
+async function showComments(postId) {
+  await getComments();
+  userPostCommentParent.style.display = "flex";
+
+  const post = posts.find(post => post.id === Number(postId));
+
+  postTitle.textContent = post.title;
+  postBody.textContent = post.body;
+
+  postCommentsUl.innerHTML = "";
+  let newCommentStructure = "";
+
+  let postComments = comments.filter((object) => {
+    return object.postId == postId;
+  });
+
+  postComments.forEach((element) => {
+    newCommentStructure += `<li>
+      <div class="user-comment-profile-info">
+        <p class="user-comment-name">${element.name}</p>
+        <p class="user-comment-email"><img src="icons/email-icon.png" /> ${element.email}</p>
+      </div>
+      <p class="user-comment">${element.body}</p>
+    </li>`;
+  });
+
+  postCommentsUl.innerHTML = newCommentStructure;
+}
